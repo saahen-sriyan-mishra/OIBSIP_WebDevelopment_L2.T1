c648f8d6875e0a3dac50d5ac7eb9d64c2f3c6320
saahen-sriyan-mishra <139043263+saahen-sriyan-mishra@users.noreply.github.com>
2023-08-04 14:31:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let display = document.getElementById('display');
+let calculationDisplay = document.getElementById('calculation');
+
+function appendToDisplay(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+  calculationDisplay.textContent = '';
+}
+
+function deleteFromDisplay() {
+  display.value = display.value.slice(0, -1);
+}
+
+function negate() {
+  display.value = display.value.startsWith('-') ? 
+      //conditions
+      display.value.slice(1) : '-' + display.value;
+}
+
+function calculate() {
+  try {
+    let expression = display.value
+      .replace(/x/g, '*')
+      .replace(/÷/g, '/') 
+      .replace(/\^/g, '**') 
+      .replace(/(\d+)%(\d+)/g, '($1/100)*$2') 
+      .replace(/%/g, '/100'); 
+
+    let result = eval(expression);
+    display.value = result;
+    calculationDisplay.textContent = expression + '=' + result;
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
